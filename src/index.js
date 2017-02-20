69d37ea845cbbd9e8f1e302f4a1d82ecf8318d1f
chris lloyd <dev@chrislloyd.me>
2017-02-19 21:58:28 -0500

    draw initial gameboard

--- src/index.js
+++ src/index.js
@@ -1,10 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+
+let getInitialGameboard = (height, width) => {
+  let board = [];
+  for(let i = 0; i < height*width; i++) {
+    board.push(0);
+  }
+  return board;
+}
+
+const BOARD_HEIGHT = 20;
+const BOARD_WIDTH = 50;
+const INITIAL_BOARD = getInitialGameboard(BOARD_HEIGHT, BOARD_WIDTH);
+
+const Row = (props) => {
+  let rows = props.cells.map((cell) => {
+    return (
+      <div className="cell"></div>
+    )
+  })
+
+  return (
+    <div className="row">
+      {rows}
+    </div>
+  )
+}
+
+const Gameboard = (props) => {
+  let board = props.board.reduce((rows, cell, index) => {
+    if(index % props.width === 0) {
+      rows.push([]);
+    }
+
+    rows[Math.floor(index/props.width)].push(cell);
+
+    return rows;
+  }, [])
+    .map((row) => {
+      return (
+        <Row cells={row}/>
+      )
+    });
+  return (
+    <div>
+      {board}
+    </div>
+  )
+}
+
+class GameOfLife extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {board: INITIAL_BOARD, height: BOARD_HEIGHT, width: BOARD_WIDTH};
+  }
+  render() {
+    return (
+      <Gameboard
+        board={this.state.board}
+        height={this.state.height}
+        width={this.state.width} />
+    )
+  }
+}
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Game of Life</h1>
+      <GameOfLife />
     )
   }
 }
