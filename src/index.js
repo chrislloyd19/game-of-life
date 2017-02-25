1ed7318e525964867bcedd6377e2c8d6355a5ac9
chris lloyd <dev@chrislloyd.me>
2017-02-25 15:33:50 -0500

    game controls

--- src/index.js
+++ src/index.js
@@ -108,39 +108,64 @@ const Gameboard = (props) => {
 class GameOfLife extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {board: INITIAL_BOARD, height: BOARD_HEIGHT, width: BOARD_WIDTH};
+    this.state = {board: INITIAL_BOARD, height: BOARD_HEIGHT, width: BOARD_WIDTH, ticks: 0};
+
+    this.tick = this.tick.bind(this);
+    this.startTicking = this.startTicking.bind(this);
+    this.stopTicking = this.stopTicking.bind(this);
+    this.clearBoard = this.clearBoard.bind(this);
   }
-  componentDidMount(){
-    let moving = setInterval( () => {
-      let board = this.state.board;
-      let width = this.state.width;
-      let newBoard = this.state.board.map(function(cell, index) {
-        let liveNeighbors = numberLiveNeighbors(board, index, width);
+  tick() {
+    let board = this.state.board;
+    let width = this.state.width;
+    let newBoard = this.state.board.map(function(cell, index) {
+      let liveNeighbors = numberLiveNeighbors(board, index, width);
 
-        if(cell === 1 && (liveNeighbors === 2 || liveNeighbors === 3)) {
-          return 1;
-        }
+      if(cell === 1 && (liveNeighbors === 2 || liveNeighbors === 3)) {
+        return 1;
+      }
 
-        if(cell === 0 && liveNeighbors === 3) {
-          return 1;
-        }
+      if(cell === 0 && liveNeighbors === 3) {
+        return 1;
+      }
 
-        return 0;
-      });
-      this.setState({board: newBoard});
-		}, 1000);
-
+      return 0;
+    });
+    this.setState({board: newBoard, ticks: this.state.ticks + 1});
+  }
+  startTicking() {
+    this.stopTicking();
+    let moving = setInterval( () => {this.tick()}, 1000);
     this.setState({moving: moving});
   }
-  componentWillUnmount() {
+  stopTicking() {
     clearInterval(this.state.moving);
+  }
+  clearBoard() {
+    this.stopTicking();
+    let clearedBoard = this.state.board.map((cell) => {
+      return 0;
+    });
+    this.setState({board: clearedBoard, ticks:0});
+  }
+  componentDidMount(){
+    this.startTicking();
+  }
+  componentWillUnmount() {
+    this.stopTicking();
   }
   render() {
     return (
-      <Gameboard
-        board={this.state.board}
-        height={this.state.height}
-        width={this.state.width} />
+      <div>
+        <Gameboard
+          board={this.state.board}
+          height={this.state.height}
+          width={this.state.width} />
+        <button onClick={this.startTicking}>Start</button>
+        <button onClick={this.stopTicking}>Stop</button>
+        <button onClick={this.clearBoard}>Clear</button>
+        <span>Generations: {this.state.ticks}</span>
+      </div>
     )
   }
 }
